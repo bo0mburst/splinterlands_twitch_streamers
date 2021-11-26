80a81d5b8eb01f6b2167a1b97cf200679b550450
Billy Sillano <billysillano@gmail.com>
2021-11-27 07:54:06 +0800

    shuffle streamer list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -119,9 +119,23 @@ export default {
       await doc.useServiceAccountAuth(CREDENTIALS);
       await doc.loadInfo();
       const sheetStreamers = doc.sheetsByTitle['streamers'];
-      this.streamers = await sheetStreamers.getRows();
+      let streamersRandomized = await sheetStreamers.getRows();
+      this.streamers = this.shuffle(streamersRandomized);
       const sheetPosts = doc.sheetsByTitle['posts'];
       this.posts = await sheetPosts.getRows();
+    },
+    shuffle(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+            // swap elements array[i] and array[j]
+            // we use "destructuring assignment" syntax to achieve that
+            // you'll find more details about that syntax in later chapters
+            // same can be written as:
+            // let t = array[i]; array[i] = array[j]; array[j] = t
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
     }
   }
 }
