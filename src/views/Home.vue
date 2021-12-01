e50f94e818799c27eb41b0d42aa749e6ba98113d
Billy Sillano <billysillano@gmail.com>
2021-12-01 12:22:48 +0800

    added loading

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,75 +1,83 @@
 <template>
-  <div class="min-vh-100 text-light">
-    <div class= "h-100 p-3 p-md-0 side-bar bg-black">
-        <div class="px-2 py-2 text-danger position-sticky top-0 bg-black text-center font-monospace fw-bold fs-3">No Sleep Gang</div>
-        <ul class="list-unstyled bg-dark">
-            <li v-for="(GoogleSpreadsheetRow, index) in streamers" :key="index">
-                <a v-bind:href="GoogleSpreadsheetRow.TWITCH" target="blank" class="text-light text-decoration-none px-3 py-2 d-flex flex-wrap align-items-start">
-                    <img :src="`https://robohash.org/${GoogleSpreadsheetRow.NAME}?size=30x30`"  class="rounded-circle border me-2">
-                    <div>
-                        {{GoogleSpreadsheetRow.NAME}}
-                        <span class="text-muted d-block text-decoration-none">@{{GoogleSpreadsheetRow.TWITCH.split('/').pop()}}</span>
-                    </div>
-                </a>
-            </li>
-        </ul>
-    </div>
-    <main class="ps-md-3 bg-black">
-        <div class="px-3 py-2">
-            <div class="latest-post text-center text-md-start">
-                <div class="mb-4 d-md-flex flex-wrap align-items-center justify-content-between">
-                    <div>
-                        <a class="px-2 text-light text-decoration-none d-inline-flex align-items-center" href="https://discord.gg/bQcva2bCXJ" target="blank" aria-label="discord">
-                            <small>NSG</small><img class="ms-2" src="@/assets/discord-logo.png" alt="" height="30">
-                        </a>
-                        <a class="px-2 text-light text-decoration-none d-inline-flex align-items-center" href="https://discord.gg/z83AaZuc6z" target="blank" aria-label="discord">
-                            <small>NSG-Show</small><img class="ms-2" src="@/assets/discord-logo.png" alt="" height="30">
-                        </a>
-                    </div>
+    <div>
+        <div v-if="isLoading" class="min-vh-100 bg-black d-flex align-items-center justify-content-center">
+            <div class="spinner-grow text-warning loading"></div>
+        </div>
 
-                    <div class="px-2 d-md-flex bg-black flex-wrap align-items-center">
-                        <div class="me-4 small mb-2 my-2">Play with us : </div>
-                        <a href="https://splinterlands.com" title="https://splinterlands.com" target="blank" class="bg-dark py-1 px-3 my-2 d-inline-block small me-2 rounded rounded-pill text-light text-decoration-none">Splinterlands</a>
-                        <a href="https://www.risingstargame.com" title="https://www.risingstargame.com" target="blank" class="bg-dark py-1 px-3 my-2 d-inline-block small me-2 rounded rounded-pill text-light text-decoration-none">Rising Stargame</a>
-                        <a href="https://fadedmonsuta.io" title="https://fadedmonsuta.io" target="blank" class="bg-dark py-1 px-3 my-2 d-inline-block small me-2 rounded rounded-pill text-light text-decoration-none">Faded Monsuta</a>
-                        <a href="https://www.streamraiders.com" title="https://www.streamraiders.com" target="blank" class="bg-dark py-1 px-3 my-2 d-inline-block small me-2 rounded rounded-pill text-light text-decoration-none">Stream Raiders</a>
-                    </div>
-                </div>
+        <transition name="fade">
+        <div  v-if="!isLoading" class="min-vh-100 text-light position-relative">
+            <div class= "h-100 p-3 p-md-0 side-bar bg-black">
+                <div class="px-2 py-2 text-danger position-sticky top-0 bg-black text-center font-monospace fw-bold fs-3">No Sleep Gang</div>
+                <ul class="list-unstyled bg-dark">
+                    <li v-for="(GoogleSpreadsheetRow, index) in streamers" :key="index">
+                        <a v-bind:href="GoogleSpreadsheetRow.TWITCH" target="blank" class="text-light text-decoration-none px-3 py-2 d-flex flex-wrap align-items-start">
+                            <img :src="`https://robohash.org/${GoogleSpreadsheetRow.NAME}?size=30x30`"  class="rounded-circle border me-2">
+                            <div>
+                                {{GoogleSpreadsheetRow.NAME}}
+                                <span class="text-muted d-block text-decoration-none">@{{GoogleSpreadsheetRow.TWITCH.split('/').pop()}}</span>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
+            </div>
 
-                <div class="row" v-if="posts.length">
-                    <div
-                        v-for="(GoogleSpreadsheetRow, index) in posts"
-                        :key="index"
-                        class="col-lg-6"
-                    >
-                        <div class="card mb-3 bg-dark post">
-                            <div class="row g-0">
-                                <div class="col-md-4 d-flex align-items-center">
-                                    <a v-bind:href="GoogleSpreadsheetRow.LINK" target="blank" class="rounded-start thumbnail text-decoration-none" :style="`background-image: url('${GoogleSpreadsheetRow.THUMBNAIL}')`" v-if="GoogleSpreadsheetRow.THUMBNAIL"></a>
-                                    <div v-else class="img-fluid p-3 text-center text-muted">No image available</div>
-                                </div>
-                                <div class="col-md-8">
-                                <div class="card-body">
-                                    <a v-bind:href="GoogleSpreadsheetRow.LINK" target="blank" class="text-decoration-none">
-                                        <p class="card-title mb-0 text-light">{{limitText(GoogleSpreadsheetRow.TITLE, 80)}}</p>
-                                        <p class="text-muted mb-0 text-nowrap text-truncate">{{GoogleSpreadsheetRow.DESCRIPTION || ''}}</p>
-                                    </a>
-                                    <div class="mt-2 text-muted small text-end">
-                                        {{GoogleSpreadsheetRow.AUTHOR}}
+            <main class="ps-md-3 bg-black">
+                <div class="px-3 py-2">
+                    <div class="latest-post text-center text-md-start">
+                        <div class="mb-4 d-md-flex flex-wrap align-items-center justify-content-between">
+                            <div>
+                                <a class="px-2 text-light text-decoration-none d-inline-flex align-items-center" href="https://discord.gg/bQcva2bCXJ" target="blank" aria-label="discord">
+                                    <small>NSG</small><img class="ms-2" src="@/assets/discord-logo.png" alt="" height="30">
+                                </a>
+                                <a class="px-2 text-light text-decoration-none d-inline-flex align-items-center" href="https://discord.gg/z83AaZuc6z" target="blank" aria-label="discord">
+                                    <small>NSG-Show</small><img class="ms-2" src="@/assets/discord-logo.png" alt="" height="30">
+                                </a>
+                            </div>
+
+                            <div class="px-2 d-md-flex bg-black flex-wrap align-items-center">
+                                <div class="me-4 small mb-2 my-2">Play with us : </div>
+                                <a href="https://splinterlands.com" title="https://splinterlands.com" target="blank" class="bg-dark py-1 px-3 my-2 d-inline-block small me-2 rounded rounded-pill text-light text-decoration-none">Splinterlands</a>
+                                <a href="https://www.risingstargame.com" title="https://www.risingstargame.com" target="blank" class="bg-dark py-1 px-3 my-2 d-inline-block small me-2 rounded rounded-pill text-light text-decoration-none">Rising Stargame</a>
+                                <a href="https://fadedmonsuta.io" title="https://fadedmonsuta.io" target="blank" class="bg-dark py-1 px-3 my-2 d-inline-block small me-2 rounded rounded-pill text-light text-decoration-none">Faded Monsuta</a>
+                                <a href="https://www.streamraiders.com" title="https://www.streamraiders.com" target="blank" class="bg-dark py-1 px-3 my-2 d-inline-block small me-2 rounded rounded-pill text-light text-decoration-none">Stream Raiders</a>
+                            </div>
+                        </div>
+                        <div class="row" v-if="posts.length">
+                            <div
+                                v-for="(GoogleSpreadsheetRow, index) in posts"
+                                :key="index"
+                                class="col-lg-6"
+                            >
+                                <div class="card mb-3 bg-dark post">
+                                    <div class="row g-0">
+                                        <div class="col-md-4 d-flex align-items-center">
+                                            <a v-bind:href="GoogleSpreadsheetRow.LINK" target="blank" class="rounded-start thumbnail text-decoration-none" :style="`background-image: url('${GoogleSpreadsheetRow.THUMBNAIL}')`" v-if="GoogleSpreadsheetRow.THUMBNAIL"></a>
+                                            <div v-else class="img-fluid p-3 text-center text-muted">No image available</div>
+                                        </div>
+                                        <div class="col-md-8">
+                                        <div class="card-body">
+                                            <a v-bind:href="GoogleSpreadsheetRow.LINK" target="blank" class="text-decoration-none">
+                                                <p class="card-title mb-0 text-light">{{limitText(GoogleSpreadsheetRow.TITLE, 80)}}</p>
+                                                <p class="text-muted mb-0 text-nowrap text-truncate">{{GoogleSpreadsheetRow.DESCRIPTION || ''}}</p>
+                                            </a>
+                                            <div class="mt-2 text-muted small text-end">
+                                                {{GoogleSpreadsheetRow.AUTHOR}}
+                                            </div>
+                                        </div>
+                                        </div>
                                     </div>
-                                </div>
                                 </div>
                             </div>
                         </div>
+                        <div v-else class="py-5">
+                            <p class="text-muted">No latest posts</p>
+                        </div>
                     </div>
                 </div>
-                <div v-else class="py-5">
-                    <p class="text-muted">No latest posts</p>
-                </div>
-            </div>
+            </main>
         </div>
-    </main>
-  </div>
+        </transition>
+    </div>
 </template>
 
 <script>
@@ -86,7 +94,8 @@ export default {
   data () {
     return {
       streamers: [],
-      posts: []
+      posts: [],
+      isLoading: true
     }
   },
 
@@ -107,14 +116,16 @@ export default {
         return split;
     },
     async loadData () {
-      const doc = new GoogleSpreadsheet(SHEET_ID);
-      await doc.useServiceAccountAuth(CREDENTIALS);
-      await doc.loadInfo();
-      const sheetStreamers = doc.sheetsByTitle['streamers'];
-      let streamersRandomized = await sheetStreamers.getRows();
-      this.streamers = this.shuffle(streamersRandomized);
-      const sheetPosts = doc.sheetsByTitle['posts'];
-      this.posts = await sheetPosts.getRows();
+        this.isLoading = true;
+        const doc = new GoogleSpreadsheet(SHEET_ID);
+        await doc.useServiceAccountAuth(CREDENTIALS);
+        await doc.loadInfo();
+        const sheetStreamers = doc.sheetsByTitle['streamers'];
+        let streamersRandomized = await sheetStreamers.getRows();
+        this.streamers = this.shuffle(streamersRandomized);
+        const sheetPosts = doc.sheetsByTitle['posts'];
+        this.posts = await sheetPosts.getRows();
+        this.isLoading = false;
     },
     shuffle(array) {
         for (let i = array.length - 1; i > 0; i--) {
@@ -173,5 +184,16 @@ export default {
     background-size: cover;
     height: 130px;
     width: 100%;
+}
+
+.loading {
+    transform: scale(2);
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
