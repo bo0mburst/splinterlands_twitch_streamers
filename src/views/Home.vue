395689718361c240a58f99e8df460ece11abcbdb
Billy Sillano <billysillano@gmail.com>
2022-10-23 21:06:54 +0800

    dynamic posts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,72 +6,72 @@
 
         <transition name="fade">
         <div  v-if="!isLoading" class="min-vh-100 text-light position-relative">
-            <div class= "h-100 p-3 p-md-0 side-bar bg-black">
-                <div class="px-2 py-2 text-danger position-sticky top-0 bg-black text-center font-monospace fw-bold fs-3">No Sleep Gang</div>
-                <ul class="list-unstyled bg-dark">
-                    <li v-for="(GoogleSpreadsheetRow, index) in streamers" :key="index">
-                        <a v-bind:href="GoogleSpreadsheetRow.TWITCH" target="blank" class="text-light text-decoration-none px-3 py-2 d-flex flex-wrap align-items-start">
-                            <img :src="`https://robohash.org/${GoogleSpreadsheetRow.NAME}?size=30x30`"  class="rounded-circle border me-2">
-                            <div>
-                                {{GoogleSpreadsheetRow.NAME}}
-                                <span class="text-muted d-block text-decoration-none">@{{GoogleSpreadsheetRow.TWITCH.split('/').pop()}}</span>
-                            </div>
-                        </a>
-                    </li>
-                </ul>
+            <div class= "h-100 d-flex flex-column p-3 p-md-0 side-bar bg-black">
+                <div class="px-2 py-2 side-bar-header text-danger bg-black text-center font-monospace fw-bold fs-3">No Sleep Gang</div>
+                <div class="h-100 flex-fill side-bar-list">
+                    <ul class="list-unstyled bg-dark">
+                        <li v-for="(GoogleSpreadsheetRow, index) in streamers" :key="index">
+                            <a v-bind:href="GoogleSpreadsheetRow.TWITCH" target="blank" class="text-light text-decoration-none px-3 py-2 d-flex flex-wrap align-items-start">
+                                <img :src="`https://robohash.org/${GoogleSpreadsheetRow.NAME}?size=30x30`"  class="rounded-circle border me-2">
+                                <div>
+                                    {{GoogleSpreadsheetRow.NAME}}
+                                    <span class="text-muted d-block text-decoration-none">@{{GoogleSpreadsheetRow.TWITCH.split('/').pop()}}</span>
+                                </div>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
 
             <main class="ps-md-3 bg-black">
-                <div class="px-3 py-2">
-                    <div class="latest-post text-center text-md-start">
-                        <div class="mb-4 d-md-flex flex-wrap align-items-center justify-content-between">
-                            <div>
-                                <a class="px-2 text-light text-decoration-none d-inline-flex align-items-center" href="https://discord.gg/bQcva2bCXJ" target="blank" aria-label="discord">
-                                    <small>NSG</small><img class="ms-2" src="@/assets/discord-logo.png" alt="" height="30">
-                                </a>
-                                <a class="px-2 text-light text-decoration-none d-inline-flex align-items-center" href="https://discord.gg/z83AaZuc6z" target="blank" aria-label="discord">
-                                    <small>NSG-Show</small><img class="ms-2" src="@/assets/discord-logo.png" alt="" height="30">
-                                </a>
+                <div class="latest-post text-center text-md-start">
+                    <div class="px-3 py-2 mb-4 d-md-flex flex-wrap align-items-center justify-content-between main-page-header bg-black">
+                        <div>
+                            <a class="px-2 text-light text-decoration-none d-inline-flex align-items-center" href="https://discord.gg/bQcva2bCXJ" target="blank" aria-label="discord">
+                                <small>NSG</small><img class="ms-2" src="@/assets/discord-logo.png" alt="" height="30">
+                            </a>
+                            <a class="px-2 text-light text-decoration-none d-inline-flex align-items-center" href="https://discord.gg/z83AaZuc6z" target="blank" aria-label="discord">
+                                <small>NSG-Show</small><img class="ms-2" src="@/assets/discord-logo.png" alt="" height="30">
+                            </a>
 
-                                <a class="px-2 text-light text-decoration-none d-inline-flex align-items-center" href="https://discord.gg/f5axpKQ8cT" target="blank" aria-label="discord">
-                                    <small>SBT Official</small><img class="ms-2" src="@/assets/discord-logo.png" alt="" height="30">
-                                </a>
-                            </div>
+                            <a class="px-2 text-light text-decoration-none d-inline-flex align-items-center" href="https://discord.gg/f5axpKQ8cT" target="blank" aria-label="discord">
+                                <small>SBT Official</small><img class="ms-2" src="@/assets/discord-logo.png" alt="" height="30">
+                            </a>
+                        </div>
 
-                            <div class="px-2 d-md-flex bg-black flex-wrap align-items-center">
-                                <div class="me-4 small mb-2 my-2">Play with us : </div>
-                                <a href="https://splinterlands.com" title="https://splinterlands.com" target="blank" class="bg-dark py-1 px-3 my-2 d-inline-block small me-2 rounded rounded-pill text-light text-decoration-none">Splinterlands</a>
-                                <a href="https://www.risingstargame.com" title="https://www.risingstargame.com" target="blank" class="bg-dark py-1 px-3 my-2 d-inline-block small me-2 rounded rounded-pill text-light text-decoration-none">Rising Stargame</a>
-                                <a href="https://fadedmonsuta.io" title="https://fadedmonsuta.io" target="blank" class="bg-dark py-1 px-3 my-2 d-inline-block small me-2 rounded rounded-pill text-light text-decoration-none">Faded Monsuta</a>
-                                <a href="https://www.streamraiders.com" title="https://www.streamraiders.com" target="blank" class="bg-dark py-1 px-3 my-2 d-inline-block small me-2 rounded rounded-pill text-light text-decoration-none">Stream Raiders</a>
-                            </div>
+                        <div class="px-2 d-md-flex bg-black flex-wrap align-items-center">
+                            <div class="me-4 small mb-2 my-2">Play with us : </div>
+                            <a href="https://splinterlands.com" title="https://splinterlands.com" target="blank" class="bg-dark py-1 px-3 my-2 d-inline-block small me-2 rounded rounded-pill text-light text-decoration-none">Splinterlands</a>
+                            <a href="https://www.risingstargame.com" title="https://www.risingstargame.com" target="blank" class="bg-dark py-1 px-3 my-2 d-inline-block small me-2 rounded rounded-pill text-light text-decoration-none">Rising Stargame</a>
+                            <a href="https://fadedmonsuta.io" title="https://fadedmonsuta.io" target="blank" class="bg-dark py-1 px-3 my-2 d-inline-block small me-2 rounded rounded-pill text-light text-decoration-none">Faded Monsuta</a>
+                            <a href="https://www.streamraiders.com" title="https://www.streamraiders.com" target="blank" class="bg-dark py-1 px-3 my-2 d-inline-block small me-2 rounded rounded-pill text-light text-decoration-none">Stream Raiders</a>
                         </div>
-                        <div class="d-flex align-items-center justify-content-center mb-5">
-                            <iframe src="https://player.twitch.tv/?channel=splinterlandstv&parent=nosleepgang.netlify.app" frameborder="0" allowfullscreen="true" scrolling="no" height="480" width="854"></iframe>
-                        </div>
-                        <div class="row" v-if="posts.length">
+                    </div>
+                    <div class="d-flex align-items-center justify-content-center mb-5 twitch-wrapper">
+                        <iframe src="https://player.twitch.tv/?channel=splinterlandstv&parent=nosleepgang.netlify.app" frameborder="0" allowfullscreen="true" scrolling="no" height="auto" width="100%"></iframe>
+                    </div>
+                    <div class="bg-darker px-3 py-2">
+                        <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3 g-4" v-if="posts.length">
                             <div
-                                v-for="(GoogleSpreadsheetRow, index) in posts"
+                                class="col"
+                                v-for="(post, index) in posts"
                                 :key="index"
-                                class="col-lg-6"
                             >
-                                <div class="card mb-3 bg-dark post">
-                                    <div class="row g-0">
-                                        <div class="col-md-4 d-flex align-items-center">
-                                            <a :href="GoogleSpreadsheetRow.LINK" target="blank" class="rounded-start thumbnail text-decoration-none" :style="`background-image: url('${GoogleSpreadsheetRow.THUMBNAIL}')`" v-if="GoogleSpreadsheetRow.THUMBNAIL"></a>
-                                            <div v-else class="img-fluid p-3 text-center text-muted">No image available</div>
-                                        </div>
-                                        <div class="col-md-8">
-                                        <div class="card-body">
-                                            <a :href="GoogleSpreadsheetRow.LINK" target="blank" class="text-decoration-none">
-                                                <p class="card-title mb-0 text-light">{{limitText(GoogleSpreadsheetRow.TITLE, 80)}}</p>
-                                                <p class="text-muted mb-0 text-nowrap text-truncate">{{GoogleSpreadsheetRow.DESCRIPTION || ''}}</p>
-                                            </a>
-                                            <div class="mt-2 text-muted small text-end">
-                                                {{GoogleSpreadsheetRow.AUTHOR}}
-                                            </div>
-                                        </div>
-                                        </div>
+                                <div class="card bg-dark h-100">
+                                    <img :src="post.image" class="card-img-top thumbnail" alt="..." v-if="post.image">
+                                    <div v-else class="text-center thumbnail d-flex align-items-center justify-content-center w-100 text-muted">
+                                        No image available
+                                    </div>
+                                    <div class="card-body">
+                                        <a href="post.url"  class="card-title text-secondary text-decoration-none" target="blank" >
+                                            <h5>{{limitText(post.title, 150)}}</h5>
+                                        </a>
+                                        <p class="card-text">{{limitText(post.description, 200) || ''}}</p>
+                                        
+                                    </div>
+                                    <div class="card-footer d-flex justify-content-between flex-wrap">
+                                        <small>Author: {{post.author}}</small>
+                                        <small class="text-muted">{{post.created}}</small>
                                     </div>
                                 </div>
                             </div>
@@ -82,6 +82,8 @@
                     </div>
                 </div>
             </main>
+
+            <p class="text-end mb-0 mt-5"><span class="small px-1 text-light bg-primary">by: bo0mburst  @nevermeltice</span></p>
         </div>
         </transition>
     </div>
@@ -127,13 +129,50 @@ export default {
         const doc = new GoogleSpreadsheet(SHEET_ID);
         await doc.useServiceAccountAuth(CREDENTIALS);
         await doc.loadInfo();
+
         const sheetStreamers = doc.sheetsByTitle['streamers'];
         let streamersRandomized = await sheetStreamers.getRows();
         this.streamers = this.shuffle(streamersRandomized);
-        const sheetPosts = doc.sheetsByTitle['posts'];
-        this.posts = await sheetPosts.getRows();
+        const sheetPeakdUsers = doc.sheetsByTitle['peakd_users'];
+        const users = await sheetPeakdUsers.getRows();
         this.isLoading = false;
+
+        for (const {USERS} of users) {
+            const res = await this.getPost(USERS);
+            if (!res.length) return
+            const post = res[0]
+            const json = post.json_metadata;
+            const image = json.image ? json.image[0] : 'https://peakd.com/static/img/peakd_logo_home.695a58db.svg';
+            const title = post.title.length > 50 ? `${post.title}...` : post.title;
+            const author = post.author;
+            const description = json.description ? json.description : '...';
+            const url = post.url;
+            const created = new Date(post.created).toDateString();
+
+            this.posts = [
+                ...this.posts,
+                {
+                    image,
+                    title,
+                    author,
+                    description,
+                    url,
+                    created,
+                }
+            ]
+        }
     },
+
+    async getPost (account) {
+        const res = await fetch("https://api.hive.blog", {
+            body: `{"jsonrpc":"2.0", "method":"bridge.get_account_posts", "params":{"sort": "blog", "account": "${account}", "limit": 1}, "id":1}`,
+            method: "POST"
+        });
+  
+        const {result} = await res.json();
+        return result;
+    },
+
     shuffle(array) {
         for (let i = array.length - 1; i > 0; i--) {
             let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
@@ -153,30 +192,14 @@ export default {
 
 <style scoped>
 
-@media (min-width: 768px) {
-    .side-bar {
-        width: 300px;
-        position: fixed;
-        left: 0;
-        top: 0;
-        bottom: 0;
-    }
-    main {
-        margin-left: 300px;
-    }
-    .post {
-        height: 130px;
-    }
+.side-bar-list {
+    overflow: auto;
+    max-height: 300px;
 }
 
-.side-bar {
-    overflow: hidden;
+.side-bar-header {
+    position: relative;
 }
-
-.side-bar:hover {
-    overflow-y: auto;
-}
-
 
 .list-unstyled > li > div:hover {
     cursor: pointer;
@@ -187,10 +210,10 @@ export default {
 }
 
 .thumbnail {
-    background-position: center;
-    background-size: cover;
-    height: 130px;
+    height: 300px;
     width: 100%;
+    object-fit: cover;
+    object-position: top;
 }
 
 .loading {
@@ -202,5 +225,46 @@ export default {
 }
 .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
+}
+
+
+@media (min-width: 768px) {
+    .main-page-header {
+        position: sticky;
+        top: 0;
+        z-index: 999;
+    }
+    .side-bar {
+        max-height: 100%;
+        width: 300px;
+        position: fixed;
+        left: 0;
+        top: 0;
+        bottom: 0;
+    }
+
+    .side-bar-list {
+        overflow: hidden;
+        max-height: unset;
+    }
+
+    .side-bar-list:hover {
+        overflow-y: auto;
+    }
+
+    .side-bar-header {
+        position: sticky;
+    }
+    main {
+        margin-left: 300px;
+    }
+    .post {
+        height: 130px;
+    }
+
+    .twitch-wrapper {
+        max-width: 1440px;
+        margin: 0 auto;
+    }
 }
 </style>
